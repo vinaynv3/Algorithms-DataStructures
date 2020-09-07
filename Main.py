2673bef9c351ae24023fcaab23ec734825b3b4a6
vinaynv3 <vinaynv987@gmail.com>
2020-09-07 14:56:52 -0400

    Set up AL & DS project

--- Main.py
+++ Main.py
@@ -1,1 +1,16 @@
-print("Welcome to Python")
+'''
+Basic Algorithm
+
+'''
+
+def bubble_sort(alist):
+    for i in range(len(alist)):
+        for j in range(len(alist)):
+            if alist[i] <= alist[j]:
+                temp = alist[i]
+                alist[i] = alist[j]
+                alist[j] = temp
+    return alist
+
+
+print(bubble_sort([56,41,1,6,11]))
